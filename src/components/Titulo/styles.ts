69335298d6c2ee5eced9b2b297f16afa62c6d153
RODHOLFO_EBAC <ro.garo87@gmail.com>
2024-02-04 09:16:54 -0300

    Criando a Seção de Projetos

--- src/components/Titulo/styles.ts
+++ src/components/Titulo/styles.ts
@@ -3,7 +3,7 @@ import { Props } from './index'
 
 export const Titulo = styled.h3<Props>`
   color: #282aa5;
-  font-size: ${(props) => props.fontSize ? props.fontSize + 'px' : '140px'};
+  font-size: ${(props) => props.fontSize ? props.fontSize + 'px' : '14px'};
   font-weight: bold;
   margin-bottom:16px;
 `
