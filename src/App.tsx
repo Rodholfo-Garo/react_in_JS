3f0935a0fd0ad89eb56d5fbdb03f666f8dc72953
RODHOLFO_EBAC <ro.garo87@gmail.com>
2024-02-04 13:19:46 -0300

    Criando troca de tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
-import EstiloGlobal, { Container } from './styles'
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+
+import Projetos from './containers/Projetos'
 import Sidebar from './containers/Sidebar'
 import Sobre from './containers/Sobre'
-import Projetos from './containers/Projetos'
+import EstiloGlobal, { Container } from './styles'
+import temaLight from './themes/ligth'
+import temaDark from './themes/dark'
 
 function App() {
+  // Criar estado para o Tema
+  const [estaUsandoTemaDark, setEstaUsandoTemaDark] = useState(false)
+
+  function trocaTema(){
+    setEstaUsandoTemaDark(!estaUsandoTemaDark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={estaUsandoTemaDark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre></Sobre>
           <Projetos></Projetos>
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
